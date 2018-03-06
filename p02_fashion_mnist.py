f26f1940353119f0be3eaf1f20f560eb8f0a63a8
Praateek Mahajan <praateekm@gmail.com>
2018-03-05 19:49:04 -0500

    Changed dropout to dropout2d in Q9/Q10

--- p02_fashion_mnist.py
+++ p02_fashion_mnist.py
@@ -228,7 +228,6 @@ class P2Q8BatchNormNet(nn.Module):
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
 
-
 class P2Q9DropoutNet(nn.Module):
     def __init__(self):
         super(P2Q9DropoutNet, self).__init__()
@@ -240,7 +239,7 @@ class P2Q9DropoutNet(nn.Module):
 
     def forward(self, x):
         x = self.batchnorm1(F.relu(F.max_pool2d(self.conv1(x), 2)))
-        x = F.dropout(x)
+        x = F.dropout2d(x, training = self.training)
         x = F.relu(F.max_pool2d(self.conv2(x), 2))
         x = x.view(-1, 320)
         x = F.relu(self.fc1(x))
@@ -260,7 +259,7 @@ class P2Q10DropoutBatchnormNet(nn.Module):
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = self.batchnorm1(F.dropout(x))
+        x = self.batchnorm1(F.dropout2d(x, training = self.training))
         x = F.relu(F.max_pool2d(self.conv2(x), 2))
         x = x.view(-1, 320)
         x = F.relu(self.fc1(x))
