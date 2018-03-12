1ee211ed760006b6e87ec363bbef000eecb0f1cd
Nitin Kumar <nkumar14@jhu.edu>
2018-03-12 17:09:12 -0400

    Added q12 and dropout default rate

--- p02_fashion_mnist.py
+++ p02_fashion_mnist.py
@@ -162,7 +162,7 @@ def prepareDatasetAndLogging(args):
 
 
 class Net(nn.Module):
-    def __init__(self, dropout_rate):
+    def __init__(self, dropout_rate=0):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
@@ -296,6 +296,22 @@ class P2Q11ExtraConvNet(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+
+class P2Q12RemoveLayerNet(nn.Module):
+    def __init__(self):
+        super(P2Q12RemoveLayerNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.fc1 = nn.Linear(320, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = x.view(-1, 320)
+        x = F.dropout(x, training=self.training)
+        x = F.relu(self.fc1(x))
         return F.log_softmax(x, dim=1)
 
 
