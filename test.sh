6b39575a999b4dcb1da172b71ebdf80fa4b676e8
Praateek Mahajan <praateekm@gmail.com>
2018-03-12 22:35:41 -0400

    ....

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
-# Question 11 -- Dropout after Batch Normalization
-python p02_fashion_mnist.py --dataset fashion_mnist --model P2Q13UltimateNet --name q13_dropout_adam  --data_dir ../data/q13 --optimizer adam --epochs 20 --save_ep 
+#Question 11 -- Dropout after Batch Normalization
+#python p02_fashion_mnist.py --dataset fashion_mnist --model P2Q13UltimateNet --name q13_dropout_adam  --data_dir ../data/q13 --lr 1e-3 --optimizer adam --epochs 20 --save_ep 
 
 python p02_fashion_mnist.py --dataset fashion_mnist --model P2Q13UltimateNet --name q13_dropout_sgd  --data_dir ../data/q13 --optimizer sgd --lr 0.1 --epochs 20 --save_ep 
+
+
+#python p02_fashion_mnist.py --dataset fashion_mnist --model P2Q13UltimateNet --name q14_dropout_adam  --data_dir ../data/q14 --optimizer adam --lr 0.1 --epochs 20
