5076600100fb522494156682a76469e26e546c31
Praateek Mahajan <praateek@masters16.cs.jhu.edu>
2018-03-12 20:27:31 -0400

    Save only the best model

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 # Question 11 -- Dropout after Batch Normalization
-python p02_fashion_mnist.py --dataset fashion_mnist --model P2Q12RemoveLayerNet --name q12_extra_dp --data_dir ../data/q12
+python p02_fashion_mnist.py --dataset fashion_mnist --model P2Q13UltimateNet --name q13_dropout_adam  --data_dir ../data/q13 --optimizer adam --epochs 20 --save_ep 
 
-
+python p02_fashion_mnist.py --dataset fashion_mnist --model P2Q13UltimateNet --name q13_dropout_sgd  --data_dir ../data/q13 --optimizer sgd --lr 0.1 --epochs 20 --save_ep 
